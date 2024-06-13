61d0dfce81db30a4dcc0a3e158611e3f579e23f8
Valber <valber@duck.com>
2024-06-13 17:27:27 -0300

    exercicio-1.go

---
+++ exercício-nível-1/exercicio-1.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
+
+// - Utilizando o operador curto de declaração, atribua estes valores às variáveis com os identificadores "x", "y", e "z".
+//     1. 42
+//     2. "James Bond"
+//     3. true
+// - Agora demonstre os valores nestas variáveis, com:
+//     1. Uma única declaração print
+//     2. Múltiplas declarações print
