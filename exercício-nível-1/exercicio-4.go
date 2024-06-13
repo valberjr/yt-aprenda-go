27f278ecd3480a129c9efb18c816b87491c7ef9a
Valber <valber@duck.com>
2024-06-13 17:30:01 -0300

    exercicio-4.go

---
+++ exercício-nível-1/exercicio-4.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type hotdog int
+
+var x hotdog
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+}
+
+//- Crie um tipo. O tipo subjacente deve ser int.
+//- Crie uma variável para este tipo, com o identificador "x", utilizando a palavra-chave var.
+//- Na função main:
+//    1. Demonstre o valor da variável "x"
+//    2. Demonstre o tipo da variável "x"
+//    3. Atribua 42 à variável "x" utilizando o operador "="
+//    4. Demonstre o valor da variável "x"
